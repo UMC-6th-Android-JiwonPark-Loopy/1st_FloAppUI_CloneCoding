267e417dd1361e0242d1011bf2ca8841715aafe3
jiwon <pjw99032986@gmail.com>
2024-04-11 06:02:48 +0900

    feat : umc 2주차 과제

--- app/src/main/java/com/example/practice/HomeFragment.kt
+++ app/src/main/java/com/example/practice/HomeFragment.kt
@@ -1,59 +1,59 @@
 package com.example.practice
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.practice.databinding.FragmentAlbumBinding
+import com.example.practice.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        onButtonClick()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun onButtonClick() {
+        with(binding) {
+            val fragment = AlbumFragment()
+            val bundle = Bundle()
+            ivAlbum1.setOnClickListener {
+                putBundle(fragment,bundle,tvAlbumName1.text.toString(), tvAlbumTitle1.text.toString())
             }
+            ivAlbum2.setOnClickListener {
+                putBundle(fragment,bundle,tvAlbumName2.text.toString(), tvAlbumTitle2.text.toString())
+            }
+            ivAlbum3.setOnClickListener {
+                putBundle(fragment,bundle,tvAlbumName3.text.toString(), tvAlbumTitle3.text.toString())
+            }
+            ivAlbum4.setOnClickListener {
+                putBundle(fragment,bundle,tvAlbumName4.text.toString(), tvAlbumTitle4.text.toString())
+            }
+            ivAlbum5.setOnClickListener {
+                putBundle(fragment, bundle,tvAlbumName5.text.toString(), tvAlbumTitle5.text.toString())
+            }
+        }
     }
+    private fun goNextPage(fr : Fragment, bundle: Bundle){
+        fr.arguments = bundle
+        activity?.supportFragmentManager!!.beginTransaction()
+            .replace(R.id.fr_main, fr)
+            .commitAllowingStateLoss()
+    }
+    private fun putBundle(fr : Fragment ,bundle : Bundle , title : String, singer : String){
+        Log.d("putBundle 값","$title $singer")
+        bundle.putString("title", title)
+        bundle.putString("singer", singer)
+        goNextPage(fr,bundle)
+    }
+
 }
--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -1,36 +1,80 @@
 package com.example.practice
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import com.example.practice.data.Song
 import com.example.practice.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    lateinit var resultIntent : ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initNavigation()
+        onClickButton()
+        setResultIntent()
 
     }
-    private fun initNavigation(){
+
+    private fun initNavigation() {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fr_main,HomeFragment())
+            .replace(R.id.fr_main, HomeFragment())
             .commitAllowingStateLoss()
 
-        binding.mainBnv.setOnItemSelectedListener{item ->
-            when(item.itemId){
+        binding.mainBnv.setOnItemSelectedListener { item ->
+            when (item.itemId) {
                 R.id.homeFragment -> {
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.fr_main,HomeFragment())
+                        .replace(R.id.fr_main, HomeFragment())
                         .commitAllowingStateLoss()
                     return@setOnItemSelectedListener true
                 }
 
-                else -> {return@setOnItemSelectedListener true}
+                else -> {
+                    return@setOnItemSelectedListener true
+                }
             }
         }
     }
+
+
+    private fun setResultIntent(){
+        resultIntent = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val returnResult = it.data?.getStringExtra(STRING_INTENT_KEY)
+                Toast.makeText(this, returnResult, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun onClickButton() {
+        val song = Song("","")
+        with(binding){
+            song.singer = mainMiniplayerSingerTv.text.toString()
+            song.title = mainMiniplayerTitleTv.text.toString()
+        }
+        binding.mainPlayerCl.setOnClickListener {
+            val intent = Intent(this,SongActivity::class.java).apply {
+                putExtra("title", song.title)
+                putExtra("singer", song.singer)
+            }
+            resultIntent.launch(intent)
+        }
+    }
+
+    companion object {
+        const val STRING_INTENT_KEY = "intent_key"
+    }
 }
+
